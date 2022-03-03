1ff6e629746e70c85b031e114403c8142a59134a
Hamdinou Mohamed lemine <leminlemin00@gmail.com>
2022-03-03 21:09:25 +0100

    Adding Mapping

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
+
 
 ReactDOM.render(
   <div>
-  <Header />
-    <Note />
     <App />
-    <Footer />
   </div>,
   document.getElementById("root")
 );
