619d852a2d7093bac96731bd337fa2a33b35739a
Hamdinou Mohamed lemine <leminlemin00@gmail.com>
2022-03-03 03:39:01 +0100

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <div>
+  <Header />
+    <Note />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Footer />
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
